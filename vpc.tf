65321b2975c1f5b1f205937e812cb32bc950dfd8
Sean Carolan <sean.carolan@hashicorp.com>
2021-05-13 16:29:02 +0000

    Added vpc module.

---
+++ vpc.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+    source  = "app.terraform.io/seanc-tf015-training/network/google"
+    version = "~> 3.0"
+
+    project_id   = var.project
+    network_name = "gaurav-network"
+
+subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+
+}
